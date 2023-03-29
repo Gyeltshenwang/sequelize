caa67c649fe6e459cd7587f2472c3f06442c0785
Gyeltshenwang <12190053.gcit@rub.edu.bt>
2023-03-29 18:21:23 +0700

    updates

--- db/postgresConnection.js
+++ db/postgresConnection.js
@@ -6,7 +6,19 @@ require('dotenv').config()
 const sequelize = new Sequelize(process.env.DATA_BASE, process.env.USER, process.env.PASSWORD, {
     host: process.env.HOST,
     dialect: 'postgres',
-    port: process.env.PORT
+    port: process.env.PORT,
+    pool: {
+        min: 0,
+        max: 7,
+        acquireTimeoutMillis: 300000,
+        createTimeoutMillis: 300000,
+        destroyTimeoutMillis: 50000,
+        idleTimeoutMillis: 300000,
+        reapIntervalMillis: 10000,
+        createRetryIntervalMillis: 2000,
+        propagateCreateError: false,
+    },
+    acquireConnectionTimeout: 60000,
 });
 
 
