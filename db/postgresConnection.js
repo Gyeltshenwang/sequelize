9951157bcf3da4f92a70836629019e75d4ae7710
Gyeltshenwang <12190053.gcit@rub.edu.bt>
2023-04-22 21:19:04 +0700

    fixed bugs

--- db/postgresConnection.js
+++ db/postgresConnection.js
@@ -3,29 +3,35 @@ require('dotenv').config()
 
 // uri connection to postgres server
 
-const sequelize = new Sequelize(process.env.DATA_BASE, process.env.USER, process.env.PASSWORD, {
-    host: process.env.HOST,
-    dialect: 'postgres',
-    port: process.env.PORT,
-    pool: {
-        min: 0,
-        max: 7,
-        acquireTimeoutMillis: 300000,
-        createTimeoutMillis: 300000,
-        destroyTimeoutMillis: 50000,
-        idleTimeoutMillis: 300000,
-        reapIntervalMillis: 10000,
-        createRetryIntervalMillis: 2000,
-        propagateCreateError: false,
-    },
-    acquireConnectionTimeout: 60000,
-});
+
+
+let sequelize;
+if (process.env.NODE_ENV === "production") {
+    sequelize = new Sequelize(process.env.DATA_BASE, process.env.USER, process.env.PASSWORD, {
+        host: process.env.HOST,
+        dialect: 'postgres',
+        port: process.env.PORT,
+
+    });
+} else {
+    sequelize = new Sequelize(
+        process.env.DATA_BASE || 'user',
+        process.env.USER ||'postgres',
+        process.env.PASSWORD ||'Khotsha',
+        {
+        host: process.env.HOST|| 'localhost',
+        dialect: process.env.DILECT ||'postgres',
+        port: process.env.PORT|| 5432,
+
+        },
+    );
+}
 const testDbConnection = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
+    try {
+        await sequelize.authenticate();
+        console.log("Connection has been established successfully.");
+    } catch (error) {
+        console.error("Unable to connect to the database:", error);
+    }
 };
 module.exports = sequelize;
